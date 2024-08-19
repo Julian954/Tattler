afed472dae0b483a65a84f850f49c4e1f5a8bb0e
Julian954 <sanchezjulian687@gmail.com>
2024-08-19 14:07:53 -0700

    add the database

--- bin/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const app = require('../app');
-
-const server = http.createServer(app);
-
-server.listen(3000);
-server.on('listening',()=>{
-    console.log('El servidor esta escuchando el puerto 3000');
-});
--- routes/api/index.js
+++
@@ -1,6 +0,0 @@
-//RUTAS API
-const router = require('express').Router();
-
-router.use('/trips',require('./trips.route'));
-
-module.exports = router;
